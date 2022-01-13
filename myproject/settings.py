cd64e4c9e64b6f38ab85a431156c3c7e18f92bc4
Albert Lei <albert.lei@hotmail.com>
2022-01-13 13:57:06 +0300

    Complete this project

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config,Csv
+from dj_database_url import parse as db_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0+9sz!0qkevfp&7u^k%_72oknd0&hirg&*t61ng7e0mi2_csz_'
+# SECRET_KEY = 'django-insecure-0+9sz!0qkevfp&7u^k%_72oknd0&hirg&*t61ng7e0mi2_csz_'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 
 # Application definition
